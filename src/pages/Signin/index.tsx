4e36a7c8c6bcb35549579960f2044ccdf628a0eb
Lucas Nogueira <lucas_snogueira@hotmail.com>
2020-05-17 19:47:47 -0300

    wip(*): insulating components

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { FiLogIn } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
 import { Container, Content, Background } from './styles';
 
 const SignIn: React.FC = () => (
@@ -12,10 +16,10 @@ const SignIn: React.FC = () => (
       <form>
         <h1>Faça seu logon</h1>
 
-        <input type="text" placeholder="E-mail" />
-        <input type="text" placeholder="Senha" />
+        <Input name="email" type="text" placeholder="E-mail" />
+        <Input name="password" type="text" placeholder="Senha" />
 
-        <button type="submit">Entrar</button>
+        <Button type="submit">Entrar</Button>
         <a href="forgot">Esqueci minha senha</a>
       </form>
 
