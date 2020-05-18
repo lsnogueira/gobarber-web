24ab34bfc04bad39cbe44e2bfe42fbc461710c22
Lucas Nogueira <lucas_snogueira@hotmail.com>
2020-05-18 01:17:13 -0300

    feat(singin): insulating components input and button

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,3 +1,20 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
-export const Container = styled.section``;
+export const Container = styled.button`
+  background: #ff9000;
+  height: 56px;
+  border-radius: 10px;
+  border: 0;
+  color: #312e38;
+  padding: 0 16px;
+  margin-top: 16px;
+  font-weight: 500;
+  transition: background-color 0.2s;
+
+  width: 100%;
+
+  &:hover {
+    background: ${shade(0.2, '#ff9000')};
+  }
+`;
