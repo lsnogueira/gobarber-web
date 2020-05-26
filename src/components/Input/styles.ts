4e5c37bd7ac0f557e71f872c44712d427cd55f23
Lucas Nogueira <lucas_snogueira@hotmail.com>
2020-05-25 22:50:43 -0300

    wip(sign-up): error tootilp

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,8 +1,10 @@
 import styled, { css } from 'styled-components';
+import Tooltip from '../Tooltip';
 
 interface ContainerProps {
   isFocused: boolean;
   isFilled: boolean;
+  isErrored: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -18,6 +20,12 @@ export const Container = styled.div<ContainerProps>`
   align-items: center;
 
   ${(props) =>
+    props.isErrored &&
+    css`
+      border-color: #d53030;
+    `}
+
+  ${(props) =>
     props.isFocused &&
     css`
       color: #ff9000;
@@ -29,6 +37,7 @@ export const Container = styled.div<ContainerProps>`
     css`
       color: #ff9000;
     `}
+
 
   & + div {
     margin-top: 8px;
@@ -47,5 +56,14 @@ export const Container = styled.div<ContainerProps>`
 
   svg {
     margin-right: 16px;
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+
+  svg {
+    margin: 0;
   }
 `;
